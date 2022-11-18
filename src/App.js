a8f56a02a4c69d8f43ed2f16c5f3f866c4a9906f
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-11-18 11:58:13 +0100

    counter app completed

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
   }
 
   const decrement =()=>{
-    if(count >0){setCount(count -1)}
+    if(count > 0){setCount(count -1)}
   }
   return (
-  <main className="App flex items-center flex-col h-screen justify-center  border-2 ">
+  <main className="App bg-slate-50 flex items-center flex-col h-screen justify-center  border-2 ">
       <section>
       <p className='mb-10 border-2 h-20 w-20 flex justify-center items-center rounded-full bg-black text-white'>{count}</p>
       </section>
       <section className='flex justify-between w-64 lg:w-96'>
       <button onClick={decrement} className="bg-red-600 text-white h-16 w-28 lg:w-40 rounded-md font-semibold">Decrement</button>
-      <button onClick={increment} className="bg-green-400 text-white h-16 w-28 lg:w-40 rounded-md font-semibold">increment</button>
+      <button onClick={increment} className="bg-green-400 text-white h-16 w-28 lg:w-40 rounded-md font-semibold">Increment</button>
       </section>
     
       
