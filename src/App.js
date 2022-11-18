784beb9656a109c1aa9c97a0006b3a89ce205d24
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-11-18 11:42:21 +0100

    counter app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment =()=>{
+    setCount(count + 1)
+  }
+
+  const decrement =()=>{
+    if(count >0){setCount(count -1)}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <main className="App flex items-center flex-col h-screen justify-center  border-2 ">
+      <section>
+      <p className='mb-10 border-2 h-20 w-20 flex justify-center items-center rounded-full bg-black text-white'>{count}</p>
+      </section>
+      <section className='flex justify-between w-64 lg:w-96'>
+      <button onClick={decrement} className="bg-red-600 text-white h-16 w-28 lg:w-40 rounded-md font-semibold">Decrement</button>
+      <button onClick={increment} className="bg-green-400 text-white h-16 w-28 lg:w-40 rounded-md font-semibold">increment</button>
+      </section>
+    
+      
+    </main>
   );
 }
 
